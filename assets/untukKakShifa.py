022bd1fef8680dd5bda5619fc3d652bb7aad90a9
risqi firdaus <16520456@std.stei.itb.ac.id>
2021-08-15 11:21:36 +0700

    fix in index, data_baru

---
+++ assets/untukKakShifa.py
@@ -0,0 +1,14 @@
+
+
+f = open("tex_lamaran.txt", "w")
+for i in range (1000):
+    f.write("Jadi Kasuh-ku Kak\n")
+
+for i in range (500):
+    f.write("ini_lamaran\n")
+
+for i in range (250):
+    f.write("shifa salsabiila\n")
+
+for i in range(200):
+    f.write("Muhammad Risqi Firdaus\n")
